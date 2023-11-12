726a87365a7dd2a9f3149afdfaa60e4fb4f6ea2e
ShaxriyorShavkatov <s.shavkatov@newuu.uz>
2023-11-12 23:56:19 +0500

    third try

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,14 +1,15 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-    /*if (consumed_water<30){
-        cost = 13.0 + (a*0.4);
-    }else if(consumed_water>30 && consumed_water<50){
-        cost = 13.0 + 30*0.4+ (consumed_water-30)*0.12;
-    }else if(consumed_water>50 && consumed_water<60){
-        cost = 13.0 + 30*0.4+ 20*0.4 +(consumed_water-50)*1.4;
-    }else if(consumed_water>60) {
-        cost = 13.0 + 30 * 0.4 + 20 * 0.4 + 10 * 1.4 + (consumed_water - 60) * 1.5;
-    }*/
+    float cost = 13; // Fixed amount
+    if (consumed_water > 60) {
+        cost += 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
+    } else if (consumed_water > 50) {
+        cost += 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
+    } else if (consumed_water > 30) {
+        cost += 30 * 0.4 + (consumed_water - 30) * 0.12;
+    } else {
+        cost += consumed_water * 0.4;
+    }
     return cost;
 }
--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,17 +2,17 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-        if (height < ((S == 'M') ? 1.7 : 1.6))
-        {
-            return "Short";
-        }
+    if (height < ((S == 'm') ? 1.7 : 1.6))
+    {
+        return "Short";
+    }
 
-        if (height < ((S == 'F') ? 1.85 : 1.75))
-        {
-            return "Normal";
-        }
+    if (height < ((S == 'm') ? 1.85 : 1.75))
+    {
+        return "Normal";
+    }
 
-        return "Tall";
+    return "Tall";
     }
     // use return to return your result
     // make use of control flow statements
