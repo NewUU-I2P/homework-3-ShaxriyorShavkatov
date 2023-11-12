e8459b2f4ac9c5d006a0da966e5b90dc47f2f95f
ShaxriyorShavkatov <s.shavkatov@newuu.uz>
2023-11-12 23:59:58 +0500

    third try

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,15 +1,14 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-    float cost = 13; // Fixed amount
-    if (consumed_water > 60) {
-        cost += 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
-    } else if (consumed_water > 50) {
-        cost += 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
-    } else if (consumed_water > 30) {
-        cost += 30 * 0.4 + (consumed_water - 30) * 0.12;
-    } else {
-        cost += consumed_water * 0.4;
+    if (consumed_water<=30){
+        cost = 13.0 + (a*0.4);
+    }else if(consumed_water>30 && consumed_water<=50){
+        cost = 13.0 + 30*0.4+ (consumed_water-30)*0.12;
+    }else if(consumed_water>50 && consumed_water<=60){
+        cost = 13.0 + 30*0.4+ 20*0.4 +(consumed_water-50)*1.4;
+    }else if(consumed_water>60) {
+        cost = 13.0 + 30 * 0.4 + 20 * 0.4 + 10 * 1.4 + (consumed_water - 60) * 1.5;
     }
     return cost;
 }
